15f34d1b492de09b7fc62139a2f625ae64246833
jandzouana <jandzouana@gmail.com>
2016-09-25 20:02:24 -0700

    portfolio

---
+++ indexanim.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $(".art_nav").mouseenter(function(){
+    $(this).find('h2').toggle();
+  });
+  $(".art_nav").mouseleave(function(){
+    $(this).find('h2').toggle();
+  });
+});
