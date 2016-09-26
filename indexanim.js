1ba4d7146660c3c0a0ff0ecf8a3d0f28a35f99fe
jandzouana <jandzouana@gmail.com>
2016-09-26 13:56:04 -0700

    update 1

--- indexanim.js
+++ indexanim.js
@@ -1,8 +1,22 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
   $(".art_nav").mouseenter(function(){
-    $(this).find('h2').toggle();
+    $(this).find('h2, h3').toggle();
   });
   $(".art_nav").mouseleave(function(){
-    $(this).find('h2').toggle();
+    $(this).find('h2, h3').toggle();
   });
 });
+*/
+
+/*$(document).ready(function(){
+	if (window.matchMedia('(min-width: 800px)').matches)
+	{
+		$(".art_nav").mouseenter(function(){
+	    $(this).find('h2, h3').toggle();
+	  });
+	  $(".art_nav").mouseleave(function(){
+	    $(this).find('h2, h3').toggle();
+	  });
+	}
+});
+*/
