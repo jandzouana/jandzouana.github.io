07503950dd766bd12f78162fed7f27bded83688d
jandzouana <jandzouana@gmail.com>
2016-10-15 02:19:12 -0700

    added menu

--- indexanim.js
+++ indexanim.js
@@ -1,22 +1,6 @@
-/*$(document).ready(function(){
-  $(".art_nav").mouseenter(function(){
-    $(this).find('h2, h3').toggle();
-  });
-  $(".art_nav").mouseleave(function(){
-    $(this).find('h2, h3').toggle();
-  });
+$(document).ready(function(){
+	$('.cont-subs').click(function(){
+		$(this).toggleClass("tap");
+		//alert("hello");
+	});
 });
-*/
-
-/*$(document).ready(function(){
-	if (window.matchMedia('(min-width: 800px)').matches)
-	{
-		$(".art_nav").mouseenter(function(){
-	    $(this).find('h2, h3').toggle();
-	  });
-	  $(".art_nav").mouseleave(function(){
-	    $(this).find('h2, h3').toggle();
-	  });
-	}
-});
-*/
